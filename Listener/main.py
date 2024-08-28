cc9835899df8f9ade43f7630fc4ce70d711828e1
QiubyZ <lessonphreaker@gmail.com>
2024-08-28 21:40:41 +0800

    test

--- Listener/main.py
+++ Listener/main.py
@@ -1,38 +1,43 @@
 from subprocess import Popen, STDOUT, PIPE
 from  time import sleep
+from os import path, getcwd, makedirs
 
 def cmd (comd):
 	test = Popen(comd, shell=True, stdout=PIPE, stderr=STDOUT)
 	return test.communicate()[0].decode("utf-8")
 
-Tulis = lambda x, t: open(x, "w").write(t)
-list_kategori  = ["global", "secure", "system"]
+def buat_folder(nama_folder):
+  folder_path = path.join(getcwd(), nama_folder)
+  if not path.exists(folder_path):
+    makedirs(folder_path)
+  return folder_path
 
-def DumpsSettings():
-	file_dump = []
-	for kategori in list_kategori:
-		isi = cmd(f"su -c settings list {kategori}")
-		Tulis(f"./cache/{kategori}.txt", isi )
-		file_dump.append(f"./cache/{kategori}.txt")
-	return file_dump
-	
-def Bacafile(path) :
-	with open(path) as e:
+def Bacafile(**var) :
+	with open(**var) as e:
 		return e.read()
+			 
+def DumpsSettings():
+	path = []
+	for kategori in ["global", "secure", "system"]:
+		piles = f"./.cache/{kategori}.txt"
+		with open(file=piles, mode="w") as tulis:
+			isi = cmd(f"su -c settings list {kategori}")
+			tulis.write(isi)
+			path.append([piles, kategori])
+	return  path
 		
 def ListeningData():
-	DumpsSettings()
-	for kategori in list_kategori:
-		berkas_dump = Bacafile(f"./cache/{kategori}.txt")
-		berkas_asli = cmd(f"su -c settings list {kategori}")
-		for dumpsNo, dumpsIsi in enumerate(berkas_dump.splitlines()):
-			    for asliNo, asliIsi in enumerate(berkas_asli.splitlines()):
-			    	if(dumpsNo == asliNo):
-			    		if(dumpsIsi != asliIsi):
-			    			print(f"{dumpsNo}{dumpsIsi} | {asliNo}. {asliIsi}")
-			    			
-			    			
-			    			
-print("Goto Settings, this will Listening Your Touch...")
-while 1:
-	ListeningData()
+	for kategori in DumpsSettings():
+		file_name = kategori[0]
+		file_kate = kategori[1]
+		berkas_dump = Bacafile(file=file_name).splitlines()
+		berkas_asli = str(cmd(f"su -c settings list {file_kate}")).splitlines()
+		for dumps, cmds in zip(berkas_dump, berkas_asli):
+			if(dumps != cmds):
+				print(f"{file_kate} {cmds} | {file_kate} {dumps} ")
+
+def main():
+	buat_folder(".cache")
+	while 1:
+		ListeningData()
+main()
